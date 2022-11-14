ce43eeda681bba185d543a61b43cca0ab5f6067c
mogmoug <88359334+mogmoug@users.noreply.github.com>
2022-11-14 05:06:19 +0000

    update

---
+++ start.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install lxqt-core
+sudo apt install tigervnc-standalone-server tigervnc-common
+echo "请先设置VNC"
+vncserver -geometry 1200x700
+echo "想要停止vnc的运行，可以输入vncserver -kill {编号}，编号例如:1"
+git clone https://github.com/novnc/noVNC
+cd noVNC/utils
+echo "按下Ctrl+C停止noVNC"
+./novnc_proxy --vnc localhost:5901
